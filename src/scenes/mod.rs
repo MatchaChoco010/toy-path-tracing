ae94b9ce5dc659e174de1dcf1234ed50489663df
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2026-04-15 18:08:45 +0900

    Add scene 11 lambert bunny lit by directional light

--- src/scenes/mod.rs
+++ src/scenes/mod.rs
@@ -1,12 +1,13 @@
 mod scene_0;
 mod scene_1;
+mod scene_10;
+mod scene_11;
 mod scene_2;
 mod scene_3;
 mod scene_4;
 mod scene_5;
 mod scene_6;
 mod scene_7;
-mod scene_10;
 mod scene_8;
 mod scene_9;
 
@@ -27,6 +28,7 @@ pub fn load_scene(scene_index: u32) -> Result<(Scene, PinholeCamera), Box<dyn Er
         8 => scene_8::create_scene_8(),
         9 => scene_9::create_scene_9(),
         10 => scene_10::create_scene_10(),
+        11 => scene_11::create_scene_11(),
         _ => scene_0::create_scene_0(),
     }
 }
