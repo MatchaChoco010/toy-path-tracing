389dd8fba04549cdb48f462ddda50e5281f9a6bf
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2026-04-15 16:23:41 +0900

    Reorganize glb assets under assets/gltf/

--- src/scenes/scene_0.rs
+++ src/scenes/scene_0.rs
@@ -26,27 +26,27 @@ pub fn create_scene_0() -> Result<(Scene, crate::camera::PinholeCamera), Box<dyn
     )));
     let light = scene.add_material(Material::Emissive(EmissiveMaterial::new(Vec3::ONE, 20.0)));
 
-    let floor_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/floor.glb"))?);
+    let floor_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/gltf/floor.glb"))?);
     scene.add_instance(floor_mesh_index, wall_gray, Mat4::IDENTITY);
-    let ceiling_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/ceiling.glb"))?);
+    let ceiling_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/gltf/ceiling.glb"))?);
     scene.add_instance(ceiling_mesh_index, wall_gray, Mat4::IDENTITY);
-    let back_wall_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/back-wall.glb"))?);
+    let back_wall_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/gltf/back-wall.glb"))?);
     scene.add_instance(back_wall_mesh_index, wall_gray, Mat4::IDENTITY);
-    let left_wall_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/left-wall.glb"))?);
+    let left_wall_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/gltf/left-wall.glb"))?);
     scene.add_instance(left_wall_mesh_index, red, Mat4::IDENTITY);
-    let right_wall_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/right-wall.glb"))?);
+    let right_wall_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/gltf/right-wall.glb"))?);
     scene.add_instance(right_wall_mesh_index, green, Mat4::IDENTITY);
-    let light_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/light.glb"))?);
+    let light_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/gltf/light.glb"))?);
     scene.add_instance(light_mesh_index, light, Mat4::IDENTITY);
 
-    let bunny = load_mesh(Path::new("assets/bunny.glb"))?;
+    let bunny = load_mesh(Path::new("assets/gltf/bunny.glb"))?;
     let bunny_pivot = Vec3::new(
         bunny.bounds.center().x,
         bunny.bounds.min.y,
         bunny.bounds.center().z,
     );
     let bunny_mesh_index = scene.add_mesh(bunny);
-    let box_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/box.glb"))?);
+    let box_mesh_index = scene.add_mesh(load_mesh(Path::new("assets/gltf/box.glb"))?);
 
     let bunny_transform = Mat4::from_translation(Vec3::new(0.72, 0.0, 0.65))
         * Mat4::from_quat(game_rotation_degrees(0.0, -28.0, 0.0))
