8f10719133fa326678c5caef402aeedf65af58f4
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2026-04-23 13:32:03 +0900

    feat: add skylight comparison scenes

---
+++ src/scenes/scene_14.rs
@@ -0,0 +1,10 @@
+use std::error::Error;
+
+use crate::{camera::PinholeCamera, scene::Scene};
+
+pub fn create_scene_14() -> Result<(Scene, PinholeCamera), Box<dyn Error>> {
+    super::scene_13::create_sky_bunny_scene(
+        "assets/sky/kloofendal_48d_partly_cloudy_puresky_4k.hdr",
+        1.0,
+    )
+}
